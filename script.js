e3eb68dc0ec0096d2e39c8da4078ed01287511e2
Gilang Gumilar <gilangmlr@gmail.com>
2017-10-31 16:39:44 +0700

    Add FastPriorityQueue library

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var ts = new TreasureHunt();
+var th = new TreasureHunt();
 
-ts.startSimulationHandler = function(data) {
+th.startSimulationHandler = function(data) {
 	$('#play-button').prop('disabled', true);
 	$('#pause-button').prop('disabled', false);
 	console.log('start simulation');
 };
 
-ts.endSimulationHandler = function(data) {
+th.endSimulationHandler = function(data) {
 	$('#play-button').prop('disabled', false);
 	$('#pause-button').prop('disabled', true);
 	console.log('end simulation');
 };
 
 $('#play-button').click(function() {
-	ts.findTreasure();
+	th.findTreasure();
 });
