8ee34352f443372c22f4d4b1af4dbbe6e3491433
Gilang Gumilar <gilangmlr@gmail.com>
2017-11-01 13:59:15 +0700

    Add FPQ, fix bugs

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ th.endSimulationHandler = function(data) {
 };
 
 $('#play-button').click(function() {
-	ts.findTreasure();
+	th.findTreasure();
 });
 
-function grid(map){
+function Grid(map){
 	this.map = map;
 	this.valid_coordinate = function(x,y){
-		if(x < 0 || y < 0  || x > map.length || y > map.length){
+		if(x < 0 || y < 0  || x >= this.map.length || y >= this.map[x].length){
 			return false;
 		}
 		return true;
@@ -27,25 +27,51 @@ function grid(map){
 	this.neighbor = function(x,y){
 		var neighbors = [];
 		if(this.valid_coordinate(x-1,y)){
-			if(!(map[x-1][y].obstacle)){
-				neighbors.push(map[x-1][y])
+			if(!(this.map[x-1][y].obstacle)){
+				neighbors.push(this.map[x-1][y])
 			}
 		}
 		if(this.valid_coordinate(x+1,y)){
-			if(!(map[x+1][y].obstacle)){
-				neighbors.push(map[x+1][y])				
+			if(!(this.map[x+1][y].obstacle)){
+				neighbors.push(this.map[x+1][y])				
 			}
 		}
 		if(this.valid_coordinate(x,y-1)){
-			if(!(map[x][y-1].obstacle)){
-				neighbors.push(map[x][y-1])			
+			if(!(this.map[x][y-1].obstacle)){
+				neighbors.push(this.map[x][y-1])			
 			}
 	
 		}
 		if(this.valid_coordinate(x,y+1)){
-			if(!(map[x][y+1].obstacle)){
-				neighbors.push(map[x][y+1])				
+			if(!(this.map[x][y+1].obstacle)){
+				neighbors.push(this.map[x][y+1])				
 			}
 		}
+
+		return neighbors;
 	};	
 }
+
+function Node(x, y, obstacle, g, h, parent) {
+	this.x = x;
+	this.y = y;
+	this.obstacle = obstacle;
+	this.g = g || 0;
+	this.h = h || 0;
+	this.parent = parent || null;
+}
+
+function heuristicComparator(a, b) {
+	return (a.g + a.h) < (b.g + b.h);
+}
+
+var map = [
+  [new Node(0, 0, true), new Node(1, 0, false), new Node(2, 0, true)],
+  [new Node(0, 1, true), new Node(1, 1, false), new Node(2, 1, true)],
+  [new Node(0, 2, true), new Node(1, 2, false), new Node(2, 2, true)],
+];
+var grid = new Grid(map);
+
+var frontier = new FastPriorityQueue(heuristicComparator);
+
+console.log(grid.neighbor(1, 1));
